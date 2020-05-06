2fcdb4e44b271958b6745111d924d7d0aa6a6a7a
john <johnpruddy@gmail.com>
2020-05-06 22:08:01 +0100

    Added new comments column

--- Tables/dbo.AWBuildVersion.sql
+++ Tables/dbo.AWBuildVersion.sql
@@ -3,7 +3,8 @@ CREATE TABLE [dbo].[AWBuildVersion]
 [SystemInformationID] [tinyint] NOT NULL IDENTITY(1, 1),
 [Database Version] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [VersionDate] [datetime] NOT NULL,
-[ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF_AWBuildVersion_ModifiedDate] DEFAULT (getdate())
+[ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF_AWBuildVersion_ModifiedDate] DEFAULT (getdate()),
+[Comments] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[AWBuildVersion] ADD CONSTRAINT [PK_AWBuildVersion_SystemInformationID] PRIMARY KEY CLUSTERED  ([SystemInformationID]) ON [PRIMARY]
